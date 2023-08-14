f69e2745962d380a04d073feff89f6f83fdb3eb1
rmanwa <robamanwa@gmail.com>
2023-08-14 23:00:14 +0000

    no c

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+#3-print_reversed_list_integer.py
+
+
+def no_c(my_string):
+    """Ommits c in strings printed"""
+    new_string = ""
+    for char in (my_string):
+        if char != 'c' and char !=:
+        (new_string) += char
+    return (new_string)
