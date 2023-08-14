9c96323175b665417b4810390ff2752d366f2dac
rmanwa <robamanwa@gmail.com>
2023-08-14 22:10:05 +0000

    reversing intergers

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+#3-print_reversed_list_integer.py
+
+
+def print_reversed_list_integer(my_list[]):
+    """Reversing a string"""
+    for i in reversed(my_list):
+        print("{d}".format(i))
