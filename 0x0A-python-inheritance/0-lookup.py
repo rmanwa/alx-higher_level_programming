582139c4baa9075e6668250a5851071854f1f65b
rmanwa <Rawbatmanwa@gmail.com>
2023-09-13 11:41:08 -0700

    Python - Inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 0-lookup
+
+"""Defines object attribute lookup function."""
+
+
+def lookup(obj):
+    """Return list of an object's available attributes."""
+    return (dir(obj))
